a0e47ef74684864752c7ddb40405b7a56c52473a
Chris-Maina <krismaish34@gmail.com>
2017-07-21 01:50:25 +0300

    Modify to import userclass

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,11 @@
 # app/__init__.py
 from flask import Flask
+from app import useraccounts
 
 # Initialize the app
 app = Flask(__name__, instance_relative_config=True)
 
+user_object = useraccounts.UserClass()
 from app import views
 
 # Load the config file
