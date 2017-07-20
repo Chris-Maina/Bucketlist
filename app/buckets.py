8c5e0c139381c9e71ab6fb4c2f2d1885024d782b
Chris-Maina <krismaish34@gmail.com>
2017-07-21 02:26:51 +0300

    Add edit function

--- app/buckets.py
+++ app/buckets.py
@@ -30,4 +30,24 @@ class BucketClass(object):
         else:
             return "No special characters (. , ! space [] )"
         return self.buckets
-    
+
+    def edit_bucket(self, edit_name, org_name):
+        """Handles edits made to bucket name
+            Args
+                editted name and original name
+            returns
+                error message or a list of buckets
+        """
+        if re.match("^[a-zA-Z0-9_]*$", edit_name):
+            for bucket in self.buckets:
+                if edit_name == bucket['name']:
+                    return "Bucket name already exists"
+                elif org_name == bucket['name']:
+                    del bucket['name']
+                    edit_dict = {
+                        'name': edit_name
+                    }
+                    bucket.update(edit_dict)
+        else:
+            return "No special characters (. , ! space [] )"
+        return self.buckets
