6ab9b6bcede3a6e5f7c110e00d45bc030a67e2fc
Chris-Maina <krismaish34@gmail.com>
2017-07-21 02:20:41 +0300

    Import bucket class

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,13 @@
 # app/__init__.py
 from flask import Flask
 from app import useraccounts
+from app import buckets
 
 # Initialize the app
 app = Flask(__name__, instance_relative_config=True)
 
 user_object = useraccounts.UserClass()
+bucket_object = buckets.BucketClass()
 from app import views
 
 # Load the config file
