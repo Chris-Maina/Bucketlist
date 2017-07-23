5827a81da9158f0b2c3895e162e1199fde5a4b1e
Chris-Maina <krismaish34@gmail.com>
2017-07-23 17:47:51 +0300

    Modify to add owner key in activity dictionary

--- app/activity.py
+++ app/activity.py
@@ -9,7 +9,7 @@ class ActivityClass(object):
         # list to hold activities within a bucket
         self.activity_list = []
 
-    def add_activity(self, bucketname, activity_name):
+    def add_activity(self, bucketname, activity_name, user):
         """Handles adding an activity to a bucket
             Args
                 bucket name
@@ -18,27 +18,31 @@ class ActivityClass(object):
         """
         # Check for special characters
         if re.match("^[a-zA-Z0-9_]*$", activity_name):
-            for item in self.activity_list:
+            # Get users activities
+            my_activities = self.owner_activities(user)
+            for item in my_activities:
                 if item['name'] == activity_name:
                     return "Activity name already exists"
             activity_dict = {
                 'name': activity_name,
-                'bucket': bucketname
+                'bucket': bucketname,
+                'owner': user
             }
             self.activity_list.append(activity_dict)
-            return self.activity_list
+            return self.owner_activities(user)
         else:
             return "No special characters (. , ! space [] )"
 
-    def edit_activity(self, activity_name, org_activity_name, bucket_name):
+    def edit_activity(self, activity_name, org_activity_name, bucket_name, user):
         """Handles editing of activities
             Args
                 editted name and original name
             returns
                 error message or a list of activities
         """
-        edit_activity_list = []
-        for activity in self.activity_list:
+        # Get users activities
+        my_activities = self.owner_activities(user)
+        for activity in my_activities:
             if activity['bucket'] == bucket_name:
                 if activity['name'] != activity_name:
                     if activity['name'] == org_activity_name:
@@ -47,21 +51,33 @@ class ActivityClass(object):
                             'name': activity_name,
                         }
                         activity.update(edit_dict)
-                        return self.activity_list
                 else:
                     return "Activity name already exists"
-        return edit_activity_list
+        return my_activities
 
-    def delete_activity(self, activity_name):
+    def delete_activity(self, activity_name, user):
         """Handles deletion of bucket activities
         Args
             activity name
         returns
             list with activity name removed
         """
-        self.activity_list = [activity for activity in self.activity_list if activity.get(
+        # Get users activities
+        my_activities = self.owner_activities(user)
+        my_activities = [activity for activity in my_activities if activity.get(
             'name') != activity_name]
         deleted_activity_list = []
-        for activity in self.activity_list:
+        for activity in my_activities:
             deleted_activity_list.append(activity['name'])
         return deleted_activity_list
+
+    def owner_activities(self, user):
+        """Returns activities belonging to a user
+        Args
+                 user
+            returns
+                 list of user's activities
+        """
+        user_activities = [item for item in self.activity_list if item['owner'] == user]
+        return user_activities
+
