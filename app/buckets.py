9051b6df846205eae57059b36b4836377fcc1a1a
Chris-Maina <krismaish34@gmail.com>
2017-07-23 17:47:24 +0300

    Modify to add owner key in bucket dictionary

--- app/buckets.py
+++ app/buckets.py
@@ -11,7 +11,7 @@ class BucketClass(object):
         # list to hold buckets a user creates
         self.buckets = []
 
-    def create_bucket(self, bucket_name):
+    def create_bucket(self, bucket_name, user):
         """Handles creation of buckets
             Args
                 bucket name
@@ -20,19 +20,22 @@ class BucketClass(object):
         """
         # Check for special characters
         if re.match("^[a-zA-Z0-9_]*$", bucket_name):
+            # Get users buckets
+            my_buckets = self.owner_buckets(user)
             # check if bucket name exists
-            for item in self.buckets:
+            for item in my_buckets:
                 if bucket_name == item['name']:
                     return "Bucket name already exists."
             bucket_dict = {
                 'name': bucket_name,
+                'owner':user,
             }
             self.buckets.append(bucket_dict)
         else:
             return "No special characters (. , ! space [] )"
-        return self.buckets
+        return self.owner_buckets(user)
 
-    def edit_bucket(self, edit_name, org_name):
+    def edit_bucket(self, edit_name, org_name, user):
         """Handles edits made to bucket name
             Args
                 editted name and original name
@@ -40,7 +43,9 @@ class BucketClass(object):
                 error message or a list of buckets
         """
         if re.match("^[a-zA-Z0-9_]*$", edit_name):
-            for bucket in self.buckets:
+            # Get users buckets
+            my_buckets = self.owner_buckets(user)
+            for bucket in my_buckets:
                 if edit_name == bucket['name']:
                     return "Bucket name already exists"
                 elif org_name == bucket['name']:
@@ -51,19 +56,31 @@ class BucketClass(object):
                     bucket.update(edit_dict)
         else:
             return "No special characters (. , ! space [] )"
-        return self.buckets
+        return self.owner_buckets(user)
 
-    def delete_bucket(self, bucket_name):
+    def delete_bucket(self, bucket_name, user):
         """Handles removal of buckets using list comprehension
             Args
                  bucket name
             returns
                  list with bucket name removed
         """
-        self.buckets = [bucket for bucket in self.buckets if bucket.get(
+        # Get users buckets
+        my_buckets = self.owner_buckets(user)
+        my_buckets = [bucket for bucket in my_buckets if bucket.get(
             'name') != bucket_name]
         # Delete activities with same bucket name
         activity_object = ActivityClass()
         activity_object.activity_list = [
             activity for activity in activity_object.activity_list if activity.get('bucketname') != bucket_name]
-        return self.buckets
+        return my_buckets
+
+    def owner_buckets(self, user):
+        """Returns buckets belonging to a user
+        Args
+                 user
+            returns
+                 list of user's bucket(s)
+        """
+        user_buckets = [item for item in self.buckets if item['owner'] == user]
+        return user_buckets
